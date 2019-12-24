ad4d41314eaf1c4ced2f8d0ce11d48d6e8ee33ba
endlmk <endlmk@users.noreply.github.com>
2019-12-24 15:49:25 +0000

    Chap.7 Pass StackTest

--- 07/VMtranslator/src/codeWriter.rs
+++ 07/VMtranslator/src/codeWriter.rs
@@ -4,6 +4,7 @@ use std::io::{self, BufWriter, Write};
 pub struct CodeWriter<W: io::Write> {
     os: io::BufWriter<W>,
     fileName_wo_ext: String,
+    index_jmp: i32,
 }
 
 impl<W: io::Write> CodeWriter<W> {
@@ -11,6 +12,7 @@ impl<W: io::Write> CodeWriter<W> {
         CodeWriter {
             os: io::BufWriter::new(writer),
             fileName_wo_ext: String::from(""),
+            index_jmp: 0
         }
     }
     pub fn setFileName(&mut self, file_name: &str) {
@@ -44,9 +46,17 @@ impl<W: io::Write> CodeWriter<W> {
     }
     
     pub fn writeArithmetic(&mut self, arg1: &str) {
-        match arg1 {
-            "add" => {  
-                let add = "\
+        let unary_template = |op: &str| format!("\
+            @SP\r\n\
+            M=M-1\r\n\
+            @SP\r\n\
+            A=M\r\n\
+            M={}M\r\n\
+            @SP\r\n\
+            M=M+1\r\n\
+            ", op);
+
+        let binary_template = |op: &str| format!("\
                 @SP\r\n\
                 M=M-1\r\n\
                 @SP\r\n\
@@ -56,16 +66,56 @@ impl<W: io::Write> CodeWriter<W> {
                 M=M-1\r\n\
                 @SP\r\n\
                 A=M\r\n\
-                D=D+M\r\n\
+                D=M{}D\r\n\
                 @SP\r\n\
                 A=M\r\n\
                 M=D\r\n\
                 @SP\r\n\
                 M=M+1\r\n\
-                ";
-                self.os.write(add.as_bytes());
-            },
-            _ => {}
+                ", op);
+
+        let cmp_template = |op: &str, i: i32| format!("\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M\r\n\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M-D\r\n\
+        @IFZERO.{1}\r\n\
+        D;{0}\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=0\r\n\
+        @END.{1}\r\n\
+        0;JMP\r\n\
+        (IFZERO.{1})\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=-1\r\n\
+        (END.{1})\r\n\
+        @SP\r\n\
+        M=M+1\r\n\
+        ", op, i);
+        
+        let asm = match arg1 {
+            "add" => binary_template("+"),
+            "sub" => binary_template("-"),
+            "neg" => unary_template("-"),
+            "eq" => { let s = cmp_template("JEQ", self.index_jmp); self.index_jmp += 1; s },
+            "gt" => { let s = cmp_template("JGT", self.index_jmp); self.index_jmp += 1; s },
+            "lt" => { let s = cmp_template("JLT", self.index_jmp); self.index_jmp += 1; s },
+            "and" => binary_template("&"),
+            "or" => binary_template("|"),
+            "not" => unary_template("!"),
+            _ => String::from(""),
+        };
+
+        if !asm.is_empty() {
+            self.os.write(asm.as_bytes());
         }
     }
 }
@@ -79,6 +129,7 @@ mod tests{
         let s = io::Cursor::new(Vec::new());
         let cw = CodeWriter::new(s);
     }
+
     #[test]
     fn setFileName() {
         let s = io::Cursor::new(Vec::new());
@@ -128,7 +179,7 @@ mod tests{
         M=M-1\r\n\
         @SP\r\n\
         A=M\r\n\
-        D=D+M\r\n\
+        D=M+D\r\n\
         @SP\r\n\
         A=M\r\n\
         M=D\r\n\
@@ -137,5 +188,297 @@ mod tests{
         ";
 
         assert_eq!(String::from_utf8(cw.os.buffer().to_vec()).unwrap(), [push_const_3, push_const_4, add].concat());
+    }
+
+    #[test]
+    fn sub() {
+        let s = io::Cursor::new(Vec::new());
+        let mut cw = CodeWriter::new(s);
+        cw.writeArithmetic("sub");
+        
+        let sub = "\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M\r\n\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M-D\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=D\r\n\
+        @SP\r\n\
+        M=M+1\r\n\
+        ";
+
+        assert_eq!(String::from_utf8(cw.os.buffer().to_vec()).unwrap(), sub);
+    }
+
+    #[test]
+    fn neg() {
+        let s = io::Cursor::new(Vec::new());
+        let mut cw = CodeWriter::new(s);
+        cw.writeArithmetic("neg");
+        
+        let neg = "\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=-M\r\n\
+        @SP\r\n\
+        M=M+1\r\n\
+        ";
+
+        assert_eq!(String::from_utf8(cw.os.buffer().to_vec()).unwrap(), neg);
+    }
+
+    #[test]
+    fn eq() {
+        let s = io::Cursor::new(Vec::new());
+        let mut cw = CodeWriter::new(s);
+        cw.writeArithmetic("eq");
+        
+        let eq = "\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M\r\n\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M-D\r\n\
+        @IFZERO.0\r\n\
+        D;JEQ\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=0\r\n\
+        @END.0\r\n\
+        0;JMP\r\n\
+        (IFZERO.0)\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=-1\r\n\
+        (END.0)\r\n\
+        @SP\r\n\
+        M=M+1\r\n\
+        ";
+
+        assert_eq!(String::from_utf8(cw.os.buffer().to_vec()).unwrap(), eq);
+    }
+
+    #[test]
+    fn gt() {
+        let s = io::Cursor::new(Vec::new());
+        let mut cw = CodeWriter::new(s);
+        cw.writeArithmetic("gt");
+        
+        let gt = "\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M\r\n\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M-D\r\n\
+        @IFZERO.0\r\n\
+        D;JGT\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=0\r\n\
+        @END.0\r\n\
+        0;JMP\r\n\
+        (IFZERO.0)\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=-1\r\n\
+        (END.0)\r\n\
+        @SP\r\n\
+        M=M+1\r\n\
+        ";
+
+        assert_eq!(String::from_utf8(cw.os.buffer().to_vec()).unwrap(), gt);
+    }
+
+    #[test]
+    fn lt() {
+        let s = io::Cursor::new(Vec::new());
+        let mut cw = CodeWriter::new(s);
+        cw.writeArithmetic("lt");
+        
+        let lt = "\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M\r\n\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M-D\r\n\
+        @IFZERO.0\r\n\
+        D;JLT\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=0\r\n\
+        @END.0\r\n\
+        0;JMP\r\n\
+        (IFZERO.0)\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=-1\r\n\
+        (END.0)\r\n\
+        @SP\r\n\
+        M=M+1\r\n\
+        ";
+
+        assert_eq!(String::from_utf8(cw.os.buffer().to_vec()).unwrap(), lt);
+    }
+
+    #[test]
+    fn and() {
+        let s = io::Cursor::new(Vec::new());
+        let mut cw = CodeWriter::new(s);
+        cw.writeArithmetic("and");
+
+        let and = "\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M\r\n\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M&D\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=D\r\n\
+        @SP\r\n\
+        M=M+1\r\n\
+        ";
+
+        assert_eq!(String::from_utf8(cw.os.buffer().to_vec()).unwrap(), and);
+    }
+
+    #[test]
+    fn or() {
+        let s = io::Cursor::new(Vec::new());
+        let mut cw = CodeWriter::new(s);
+        cw.writeArithmetic("or");
+
+        let or = "\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M\r\n\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M|D\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=D\r\n\
+        @SP\r\n\
+        M=M+1\r\n\
+        ";
+
+        assert_eq!(String::from_utf8(cw.os.buffer().to_vec()).unwrap(), or);
+    }
+
+    #[test]
+    fn not() {
+        let s = io::Cursor::new(Vec::new());
+        let mut cw = CodeWriter::new(s);
+        cw.writeArithmetic("not");
+        
+        let not = "\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=!M\r\n\
+        @SP\r\n\
+        M=M+1\r\n\
+        ";
+
+        assert_eq!(String::from_utf8(cw.os.buffer().to_vec()).unwrap(), not);
+    }
+
+
+    #[test]
+    fn rep_cmp() {
+        let s = io::Cursor::new(Vec::new());
+        let mut cw = CodeWriter::new(s);
+        cw.writeArithmetic("lt");
+        cw.writeArithmetic("lt");
+        
+        let lt1 = "\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M\r\n\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M-D\r\n\
+        @IFZERO.0\r\n\
+        D;JLT\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=0\r\n\
+        @END.0\r\n\
+        0;JMP\r\n\
+        (IFZERO.0)\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=-1\r\n\
+        (END.0)\r\n\
+        @SP\r\n\
+        M=M+1\r\n\
+        ";
+
+        let lt2 = "\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M\r\n\
+        @SP\r\n\
+        M=M-1\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        D=M-D\r\n\
+        @IFZERO.1\r\n\
+        D;JLT\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=0\r\n\
+        @END.1\r\n\
+        0;JMP\r\n\
+        (IFZERO.1)\r\n\
+        @SP\r\n\
+        A=M\r\n\
+        M=-1\r\n\
+        (END.1)\r\n\
+        @SP\r\n\
+        M=M+1\r\n\
+        ";
+
+        assert_eq!(String::from_utf8(cw.os.buffer().to_vec()).unwrap(), [lt1, lt2].concat());
     }
 }
